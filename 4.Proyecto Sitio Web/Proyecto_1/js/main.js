d7e02fd6a1ce1d608568b1f0f6378698335db471
jonatan52 <tarriba52@gmail.com>
2023-02-09 03:12:29 -0500

    Agregando imagenes y estilos al pagina de inicio

---
+++ 4.Proyecto Sitio Web/Proyecto_1/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).on('scroll', function(){
+        let posicion = $('#pr').offset();
+        if($(window).scrollTop() >= posicion.top){
+            $('header').removeClass('transparente');
+        }else{
+            $('header').addClass('transparente');
+        }
+    })
+
+    $('#burger').click(function(){
+        $('#burger').toggleClass('abierto');
+        $('.menu').toggleClass('abierto');
+    })
+})
